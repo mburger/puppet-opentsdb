a3b5231a809be7ba47e7e6407d5e087879954585
Markus Burger <markus.burger@uni-ak.ac.at>
2013-08-29 13:00:53 +0200

    minor fixes
    Change-Id: I0aaacedd1196cb89ded67a7365f9e7349ba586d9

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,20 +1,17 @@
 class opentsdb::config ( ) {
-  
+
   file {
-    
-    '/opt/opentsdb/opentsdb.conf':
-      ensure  => present, 
+    '/etc/opentsdb/opentsdb.conf':
+      ensure  => present,
       owner   => root,
       group   => root,
       mode    => 644,
       source  => 'puppet:///modules/opentsdb/opentsdb.conf';
   }
-  
+
   exec {
     'create_tables':
       command => 'env COMPRESSION=NONE HBASE_HOME=/opt/hbase /usr/share/opentsdb/bin/create_table.sh',
       unless  => 'echo "exists \'tsdb\'" | /opt/hbase/bin/hbase shell | grep "does exists"'
   }
-  
-  
 }
--- manifests/install.pp
+++ manifests/install.pp
@@ -1,13 +1,13 @@
 class opentsdb::install ( ) {
-  
+
   file {
     '/opt/opentsdb-current.deb':
       source => 'puppet:///modules/opentsdb/optsdb-2.0.0_all.deb'
   }
-  
+
   package {
     'opentsdb':
-      ensure => present,
+      ensure => latest,
       source => '/opt/opentsdb-current.deb',
       provider => 'dpkg',
       require => File['/opt/opentsdb-current.deb']
